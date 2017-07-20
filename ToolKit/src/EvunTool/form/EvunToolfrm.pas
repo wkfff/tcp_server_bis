a29be6c429b5d7ee1276a3f90af5fdbcf2f98aad
yslmyr <yslmyr@sina.com>
2017-07-20 17:23:09 +0800

    complate send http message

--- ToolKit/src/EvunTool/form/EvunToolfrm.pas
+++ ToolKit/src/EvunTool/form/EvunToolfrm.pas
@@ -8,38 +8,43 @@ uses
   System.SysUtils,
   System.Variants,
   System.Classes,
-  System.Generics.Collections,
   Vcl.Graphics,
   Vcl.Controls,
   Vcl.Forms,
   Vcl.Dialogs,
-  Vcl.Themes,
-  Vcl.Styles,
-  Vcl.SysStyles,
+  qstring,
   qplugins_base,
   QPlugins,
-  qstring,
-  qplugins_formsvc,
-  qplugins_vcl_messages,
   qplugins_vcl_formsvc,
-  Vcl.StdCtrls,
+  qplugins_vcl_messages,
+  qplugins_params,
   Vcl.ExtCtrls,
   RzPanel,
   RzSplit,
-  SynEdit,
   System.ImageList,
   Vcl.ImgList,
   cxGraphics,
   RzTabs,
+  SynEdit,
   RzButton,
   RzRadChk,
+  JvImageList,
+  SynEditHighlighter,
+  SynHighlighterIni,
+  SynHighlighterXML,
+  VirtualTrees,
+  qxml,
+  uShareMemServer,
+  System.Actions,
+  Vcl.ActnList,
+  Vcl.Menus,
+  virtualstringtreefram,
+  Vcl.StdCtrls,
   RzLabel,
   Vcl.Mask,
   RzEdit,
-  RzBtnEdt,
-  uShareMemServer,
-  qworker,
-  VirtualTrees;
+  iocp.Http.Client,
+  qworker;
 
 type
   PDMSDebugInfo = ^TDMSDebugInfo;
@@ -53,29 +58,47 @@ type
     ReceiveXML: WideString;
   end;
 
-  TfrmEvunTool = class(TForm, IQNotify)
-    rzsplEvunTool: TRzSplitter;
-    tlbrAction: TRzToolbar;
-    pnlServer: TRzPanel;
-    pgcResult: TRzPageControl;
-    tbsXML: TRzTabSheet;
-    imgSmall_16X16: TcxImageList;
+  TfrmEvunTool = class(TForm)
+    splEvunTool: TRzSplitter;
+    tbrButtons: TRzToolbar;
     sedtArgus: TSynEdit;
+    pgcResults: TRzPageControl;
+    tstXML: TRzTabSheet;
+    cbxStart: TRzCheckBox;
+    il16X16: TJvImageList;
+    rzpnlServer: TRzPanel;
     sedtXML: TSynEdit;
-    cbxBegin: TRzCheckBox;
-    scr1: TRzSpacer;
-    btnExcute: TRzToolButton;
+    sxsXML: TSynXMLSyn;
+    pcr1: TRzSpacer;
     btnList: TRzToolButton;
     btnLog: TRzToolButton;
-    btnServer: TRzButtonEdit;
+    btnExcute: TRzToolButton;
+    rzpnlList: TRzPanel;
+    vstMethodList: TVirtualStringTree;
+    actEvunTool: TActionList;
+    actStartListener: TAction;
+    sisIni: TSynIniSyn;
+    actClearArgus: TAction;
+    pmArgus: TPopupMenu;
+    pmArgusList: TPopupMenu;
+    pmXML: TPopupMenu;
+    mniClearArgus: TMenuItem;
+    actClearArgusedt: TAction;
+    mniClearArgusedt: TMenuItem;
+    actResolveXML: TAction;
+    mniResolveXML: TMenuItem;
+    mniN1: TMenuItem;
+    actClearXML: TAction;
+    mniClearXML: TMenuItem;
     lblServer: TRzLabel;
+    edtServer: TRzEdit;
     btbtnExcute: TRzBitBtn;
-    pnlList: TRzPanel;
-    vst1: TVirtualStringTree;
-    procedure btn1Click(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
-    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    actExcuteHttp: TAction;
+    procedure FormResize(Sender: TObject);
     procedure btnListClick(Sender: TObject);
+    procedure vstMethodListDblClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
     procedure vstMethodListFocusChanged(Sender: TBaseVirtualTree; Node:
       PVirtualNode; Column: TColumnIndex);
     procedure vstMethodListFreeNode(Sender: TBaseVirtualTree; Node: PVirtualNode);
@@ -85,40 +108,206 @@ type
       PVirtualNode; var ChildCount: Cardinal);
     procedure vstMethodListInitNode(Sender: TBaseVirtualTree; ParentNode, Node:
       PVirtualNode; var InitialStates: TVirtualNodeInitStates);
+    procedure actStartListenerExecute(Sender: TObject);
+    procedure actClearArgusExecute(Sender: TObject);
+    procedure actClearArgusedtExecute(Sender: TObject);
+    procedure actClearXMLExecute(Sender: TObject);
+    procedure actResolveXMLExecute(Sender: TObject);
+    procedure pgcResultsClose(Sender: TObject; var AllowClose: Boolean);
+    procedure FormShow(Sender: TObject);
+    procedure actExcuteHttpExecute(Sender: TObject);
   private
-    { Private declarations }
-    FNotifyIds: TDictionary<string, Integer>;
+    FNotifyId_log: Integer;
     FShareMem: TShareMemServer;
     FDMSInfoList: TStringList;
     FDCSInfoList: TStringList;
     procedure DoGetShareMemData(AJob: PQJob);
     procedure DoRefrashVST(AVST: TVirtualStringTree);
-  protected
-    procedure Notify(const AId: Cardinal; AParams: IQParams; var AFireNext:
-      Boolean); stdcall;
+    procedure ClearTabControl;
+    procedure DoLogMessage(AMessage: PChar);
+    procedure Log(AMessage: string);
   public
     { Public declarations }
-    destructor Destroy; override;
   end;
-
-const
-  NOTIFY_ID_CHANGE_VCL_STYLE = 'CHANGE_VCL_STYLE';
-  ServiceName: PWideChar = 'EvunTool';
-  ServiceId: TGuid = '{76397A4F-5350-498D-9CE3-AAF3F9CDAB31}';
-  Debug_MapMem: string = '__Debug_MapMem_v_x_MapMem__';
 
 implementation
 
+const
+  Debug_MapMem: string = '__Debug_MapMem_v_x_MapMem__';
+
+
 {$R *.dfm}
 
-procedure TfrmEvunTool.btn1Click(Sender: TObject);
+procedure TfrmEvunTool.actClearArgusedtExecute(Sender: TObject);
 begin
-  Self.Close;
+  sedtArgus.Clear;
 end;
 
-procedure TfrmEvunTool.FormClose(Sender: TObject; var Action: TCloseAction);
+procedure TfrmEvunTool.actClearArgusExecute(Sender: TObject);
+var
+  I: Integer;
 begin
-  Action := caFree;
+  ClearTabControl;
+  sedtXML.Clear;
+  sedtArgus.Clear;
+  for I := 0 to FDCSInfoList.Count - 1 do
+    Dispose(PDMSDebugInfo(FDCSInfoList.Objects[I]));
+  FDCSInfoList.Clear;
+
+  for I := 0 to FDCSInfoList.Count - 1 do
+    Dispose(PDMSDebugInfo(FDMSInfoList.Objects[I]));
+  FDMSInfoList.Clear;
+
+  DoRefrashVST(vstMethodList);
+end;
+
+procedure TfrmEvunTool.actClearXMLExecute(Sender: TObject);
+begin
+  ClearTabControl;
+  sedtXML.Clear;
+end;
+
+procedure TfrmEvunTool.actExcuteHttpExecute(Sender: TObject);
+var
+  AHeader: THttpHeaders;
+  AData: string;
+  I: Integer;
+  AEnd: Integer;
+  AStart: Integer;
+begin
+  AHeader := nil;
+
+  try
+    AHeader := THttpHeaders.Create;
+    AHeader.Add('Content-Encoding','gzip');
+    AHeader.Add('Content-Type','application/x-www-form-urlencoded');
+    AHeader.Add('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8');
+    AHeader.Add('Accept-Encoding', 'gzip, deflate, identity');
+    AHeader.Add('User-Agent', 'UserClient');
+    AData := sedtArgus.SelText;
+
+    if Trim(AData) = '' then
+    begin
+      for I := sedtArgus.CaretY - 1 to sedtArgus.Lines.Count - 1 do
+      begin
+        if (Trim(sedtArgus.Lines[I]) = '')  then
+        begin
+          AEnd := I - 1;
+          Break;
+        end
+        else if (I = sedtArgus.Lines.Count - 1) then
+        begin
+          AEnd := I;
+          Break;
+        end;
+      end;
+      for I := AEnd downto 0 do
+      begin
+        if (Trim(sedtArgus.Lines[I]) = '') then
+          Break;
+        if AData = '' then
+          AData := sedtArgus.Lines[I]
+        else
+          AData := sedtArgus.Lines[I] + '&' + AData;
+      end;
+    end
+    else
+    begin
+      for I := sedtArgus.BlockEnd.Line downto sedtArgus.BlockBegin.Line do
+      begin
+        if Trim(sedtArgus.Lines[I]) = '' then
+          Continue;
+        if AData = '' then
+          AData := sedtArgus.Lines[I]
+        else
+          AData := sedtArgus.Lines[I] + '&' + AData;
+      end;
+    end;
+
+    try
+      sedtXML.Text := HttpPost(edtServer.Text, AData, hct_GB2312, AHeader);
+      actResolveXML.Execute;
+      pgcResults.ActivePageIndex := 0;
+      if pgcResults.PageCount > 1 then
+        pgcResults.ActivePageIndex := pgcResults.PageCount - 1;
+    except
+      on E: Exception do
+      begin
+        ShowMessage(E.Message);
+        Log('Eorr:' + E.Message);
+      end;
+    end;
+  finally
+    FreeAndNil(AHeader);
+  end;
+end;
+
+procedure TfrmEvunTool.actResolveXMLExecute(Sender: TObject);
+var
+  rootXML: TQXML;
+  BodyXML: TQXML;
+  DataSetXML: TQXML;
+  I: Integer;
+  APage: TRzTabSheet;
+  AFrame: Tfrmvirtualstringtree;
+begin
+  rootXML := nil;
+  ClearTabControl;
+  try
+    rootXML := TQXML.Create;
+    rootXML.Parse(sedtXML.Text);
+    if rootXML.Count < 1 then
+      Exit;
+    BodyXML := rootXML.ItemByPath('RESPONSE.BODY');
+    if BodyXML.Count < 1 then
+      Exit;
+    for I := 0 to BodyXML.Count - 1 do
+    begin
+      DataSetXML := BodyXML.Items[I];
+      if (not Assigned(DataSetXML)) or (DataSetXML.Count < 1) or (DataSetXML.Attrs.Count
+        < 1) then
+        Continue;
+
+      APage := TRzTabSheet.Create(pgcResults);
+      APage.PageControl := pgcResults;
+      APage.Caption := DataSetXML.Attrs.ItemByName('NAME').Value;
+
+      AFrame := Tfrmvirtualstringtree.Create(APage);
+      AFrame.Parent := APage;
+      AFrame.DataSetXML := DataSetXML;
+
+      APage.Data := Pointer(APage);
+      APage.ImageIndex := 45;
+    end;
+    pgcResults.ActivePageIndex := 0;
+    if pgcResults.PageCount > 1 then
+      pgcResults.ActivePageIndex := pgcResults.PageCount - 1;
+  finally
+    FreeAndNil(rootXML);
+  end;
+end;
+
+procedure TfrmEvunTool.actStartListenerExecute(Sender: TObject);
+begin
+  FShareMem.Active := cbxStart.Checked;
+end;
+
+procedure TfrmEvunTool.btnListClick(Sender: TObject);
+begin
+  if rzpnlList.Showing then
+    rzpnlList.Hide
+  else
+    rzpnlList.Show;
+end;
+
+procedure TfrmEvunTool.DoLogMessage(AMessage: PChar);
+begin
+  (PluginsManager as IQNotifyManager).Send(FNotifyId_log, NewParams([AMessage]));
+end;
+
+procedure TfrmEvunTool.Log(AMessage: string);
+begin
+  DoLogMessage(PChar(AMessage));
 end;
 
 procedure TfrmEvunTool.DoGetShareMemData(AJob: PQJob);
@@ -194,13 +383,13 @@ begin
     aData := PDMSDebugInfo(aInfoList.Objects[I]);
     aData.ReceiveXML := pReceive;
 
-//    DoRefrashVST(vstMethodList);
-//    if IsDms then
-//      vstMethodList.ScrollIntoView((vstMethodList.RootNode.LastChild.LastChild),
-//        True)
-//    else
-//      vstMethodList.ScrollIntoView((vstMethodList.RootNode.FirstChild.LastChild),
-//        True);
+    DoRefrashVST(vstMethodList);
+    if IsDms then
+      vstMethodList.ScrollIntoView((vstMethodList.RootNode.LastChild.LastChild),
+        True)
+    else
+      vstMethodList.ScrollIntoView((vstMethodList.RootNode.FirstChild.LastChild),
+        True);
 
   end;
 
@@ -241,53 +430,57 @@ begin
 end;
 
 procedure TfrmEvunTool.FormCreate(Sender: TObject);
-var
-  ANotifyMgr: IQNotifyManager;
 begin
-  FNotifyIds := TDictionary<string, Integer>.Create;
-  TStyleManager.SystemHooks := TStyleManager.SystemHooks - [shMenus];
-  TStyleManager.TrySetStyle('Iceberg Classico', False);
-  ANotifyMgr := PluginsManager as IQNotifyManager;
-  FNotifyIds.Add(NOTIFY_ID_CHANGE_VCL_STYLE, ANotifyMgr.IdByName(NOTIFY_ID_CHANGE_VCL_STYLE));
-  ANotifyMgr.Subscribe(FNotifyIds.Items[NOTIFY_ID_CHANGE_VCL_STYLE], Self);
+  FShareMem := TShareMemServer.Create(Debug_MapMem);
+  FShareMem.OnGetData := DoGetShareMemData;
 
   FDMSInfoList := TStringList.Create;
   FDCSInfoList := TStringList.Create;
-  FShareMem := TShareMemServer.Create(Debug_MapMem);
-  FShareMem.OnGetData := DoGetShareMemData;
+
+  vstMethodList.NodeDataSize := SizeOf(TDMSDebugInfo);
+  vstMethodList.RootNodeCount := 2;
+
+  FNotifyId_log := (PluginsManager as IQNotifyManager).IdByName('__safe_Logger__');
+  rzpnlList.Hide;
 end;
 
-procedure TfrmEvunTool.btnListClick(Sender: TObject);
-begin
-  pnlList.Visible := True;
-end;
-
-destructor TfrmEvunTool.Destroy;
+procedure TfrmEvunTool.FormDestroy(Sender: TObject);
 var
-  ANotifyMgr: IQNotifyManager;
   I: Integer;
 begin
-  Workers.Clear(FShareMem, -1, True);
-  FShareMem.Free;
   for I := 0 to FDCSInfoList.Count - 1 do
     Dispose(PDMSDebugInfo(FDCSInfoList.Objects[I]));
   FDCSInfoList.Free;
   for I := 0 to FDMSInfoList.Count - 1 do
     Dispose(PDMSDebugInfo(FDMSInfoList.Objects[I]));
   FDMSInfoList.Free;
-  ANotifyMgr := PluginsManager as IQNotifyManager;
-  ANotifyMgr.Unsubscribe(FNotifyIds.Items[NOTIFY_ID_CHANGE_VCL_STYLE], Self);
-  FNotifyIds.Free;
-  inherited;
+
+  Workers.Clear(FShareMem, -1, True);
+  FShareMem.Free;
 end;
 
-procedure TfrmEvunTool.Notify(const AId: Cardinal; AParams: IQParams; var
-  AFireNext: Boolean);
+procedure TfrmEvunTool.FormResize(Sender: TObject);
 begin
-  if AId = FNotifyIds.Items[NOTIFY_ID_CHANGE_VCL_STYLE] then
-  begin
-    TStyleManager.TrySetStyle(AParams.ByName('VCLStyleName').AsString.Value, False);
-  end;
+  splEvunTool.UsePercent := False;
+  splEvunTool.Percent := 50;
+  splEvunTool.UsePercent := True;
+end;
+
+procedure TfrmEvunTool.FormShow(Sender: TObject);
+begin
+  sedtArgus.SetFocus;
+end;
+
+procedure TfrmEvunTool.pgcResultsClose(Sender: TObject; var AllowClose: Boolean);
+begin
+  AllowClose := pgcResults.ActivePageIndex <> 0;
+  if not AllowClose then
+    sedtXML.Text := '';
+end;
+
+procedure TfrmEvunTool.vstMethodListDblClick(Sender: TObject);
+begin
+  rzpnlList.Hide;
 end;
 
 procedure TfrmEvunTool.vstMethodListFocusChanged(Sender: TBaseVirtualTree; Node:
@@ -303,11 +496,12 @@ begin
       1:
         aInfoList := FDMSInfoList;
     end;
-//    ClearTabControl;
+    ClearTabControl;
     sedtXML.Text := PDMSDebugInfo(aInfoList.Objects[Node.Index]).ReceiveXML;
     sedtArgus.Text := sedtArgus.Text + PDMSDebugInfo(aInfoList.Objects[Node.Index]).PostArgus
       + #13 + #10 + #13 + #10 + #13 + #10;
     sedtArgus.GotoLineAndCenter(sedtArgus.Lines.Count - 1);
+    pgcResults.ActivePageIndex := 0;
   end;
 end;
 
@@ -388,6 +582,15 @@ begin
     PDMSDebugInfo(Sender.GetNodeData(Node)).PostMethod := PDMSDebugInfo(aInfoList.Objects
       [Node.Index]).PostMethod;
   end;
+end;
+
+procedure TfrmEvunTool.ClearTabControl;
+var
+  I: Integer;
+begin
+  if pgcResults.PageCount > 1 then
+    for I := pgcResults.PageCount - 1 downto 1 do
+      pgcResults.Pages[I].Free;
 end;
 
 initialization
